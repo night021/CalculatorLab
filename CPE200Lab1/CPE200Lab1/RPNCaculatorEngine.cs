b48d8172ad4da6f5a1ba58a9a54008b7eee20b2a
night021 <night.wootthipong@hotmail.com>
2017-09-05 17:38:50 +0700

    edit RPNCal

---
+++ CPE200Lab1/CPE200Lab1/RPNCaculatorEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public string Process(string str)
+        {
+            List<string> parts = str.Split(' ').ToList<string>();
+            string result = "0";
+            Stack rpnStack = new Stack();
+            for (int i =0; i < parts.Count; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    rpnStack.Push(parts[i]);
+                }
+                if (isOperator(parts[i]))
+                {
+                    string second = rpnStack.Pop().ToString();
+                    string first = rpnStack.Pop().ToString();
+                    result = calculate(parts[i], first, second);
+                    rpnStack.Push(result);
+                    
+                }
+            }
+            return result;
+        }
+    }
+}
